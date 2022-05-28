9a2332edc6335deb5fda04adb7b117e779daaa7a
tien <tien.codeby@gmail.com>
2022-05-28 16:22:23 +0700

    add wp

--- inits/wordpress.sh
+++ inits/wordpress.sh
@@ -24,10 +24,10 @@ add_wpsite_ubuntu(){
   printf "$(UI.Color.Yellow)DB user (root):$(UI.Color.Default)"; read dbuser
   printf "$(UI.Color.Yellow)DB pass (root):$(UI.Color.Default)"; read dbpass
   DB="${DOMAIN/\./_}_db"
-  cd /usr/local/lsws
-  mkdir $DOMAIN
-  mkdir $DOMAIN/{conf,html,logs}
-  wp core download --path=$DOMAIN/html --locale=en_US --allow-root; cd $DOMAIN/html
+  domainPath=/usr/local/lsws/$DOMAIN
+  rm -rf $domainPath
+  mkdir -p $domainPath/{conf,html,logs}
+  wp core download --path=$domainPath/html --locale=en_US --allow-root; cd $domainPath/html
   wp config create --dbname=$DB --dbuser=$dbuser --dbpass=$dbpass
   wp db create
   wp core install --url=$DOMAIN --title="$DOMAIN title" --admin_user=tienwp_asd --admin_password=tienwp_asd --admin_email=tien.wordpress@gmail.com
