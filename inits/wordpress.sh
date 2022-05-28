2d8fc3e85f4234e7af5080192b42dc17a541f864
tien <tien.codeby@gmail.com>
2022-05-28 15:48:29 +0700

    init wordpress

---
+++ inits/wordpress.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+init_wpcli(){
+  curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+  php wp-cli.phar --info
+  chmod +x wp-cli.phar
+  sudo mv wp-cli.phar /usr/local/bin/wp
+}
+
+add_wpsite(){
+  printf "$(UI.Color.Yellow)Domain (webb.vn):$(UI.Color.Default)"; read DOMAIN
+  printf "$(UI.Color.Yellow)DB user (root):$(UI.Color.Default)"; read dbuser
+  printf "$(UI.Color.Yellow)DB pass (root):$(UI.Color.Default)"; read dbpass
+  DB="${DOMAIN/\./_}_db"
+  # printf "$DOMAIN $DB"
+  # read -p "" fackEnterKey
+  wp core download --path=$DOMAIN --locale=en_US; cd $DOMAIN
+  wp config create --dbname=$DB --dbuser=$dbuser --dbpass=$dbpass
+  wp db create
+  wp core install --url=$DOMAIN --title="$DOMAIN title" --admin_user=tienwp_asd --admin_password=tienwp_asd --admin_email=tien.wordpress@gmail.com
+  read -p "" fackEnterKey
+}
--- menu.sh
+++ menu.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/lib/oo-bootstrap.sh"
-import inits/wp-cli
 import util/log
 import util/tryCatch
 import util/exception
+import inits/wordpress
 
 show_menus(){
   COLUMNS=$(tput cols)
@@ -13,7 +13,7 @@ show_menus(){
   {
     printf "$(UI.Color.Yellow)WORDPRESS$(UI.Color.Default)\n"
     printf "1) Init wp-cli\n"
-    printf "2) Option 2\n"
+    printf "2) Add wordpress site\n"
     printf "3) Option 3\n"
     printf "4) Option 4\n"
     printf "$(UI.Color.Red)0) Exit$(UI.Color.Default)"
@@ -22,20 +22,13 @@ show_menus(){
 
 
 read_options(){
-  # try {
-    local choice
-    read -p "Enter choice: " choice
-    case $choice in
-      1) init_wpcli1;;
-      0) exit 0;;
-      *) printf "$(UI.Color.Red)Error...$(UI.Color.Default)" && sleep 2
-    esac
-  # } catch {
-  #   echo "Caught Exception:$(UI.Color.Red) $__BACKTRACE_COMMAND__ $(UI.Color.Default)"
-  #   echo "File: $__BACKTRACE_SOURCE__, Line: $__BACKTRACE_LINE__"
-  #   Exception::PrintException "${__EXCEPTION__[@]}"
-  #   read -p "" fackEnterKey
-  # }
+  read -p "Enter choice: " choice
+  case $choice in
+    1) init_wpcli;;
+    2) add_wpsite;;
+    0) exit 0;;
+    *) printf "$(UI.Color.Red)Error...$(UI.Color.Default)" && sleep 2
+  esac
 }
 
 try {
