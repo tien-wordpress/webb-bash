1b7fcb55ab4454fb98f682acaa5ad55267ed4bfd
tien <tien.codeby@gmail.com>
2022-05-28 14:11:45 +0700

    menu

--- menu.sh
+++ menu.sh
@@ -1,76 +1,52 @@
 #!/usr/bin/env bash
+source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/lib/oo-bootstrap.sh"
+import inits/wp-cli
+import util/log
+import util/tryCatch
+import util/exception
 
-COLUMNS=$(tput cols)
-
-source "inits/wordpress.sh"
-
-main_menu(){
-    printf "    Webb.VN Menu\n"
-    printf "\n"
+show_menus(){
+  COLUMNS=$(tput cols)
+  clear
+  printf "WEBB.VN MENU\n"
+  printf "\n"
   {
-    printf "    1) Init wordpress\n"
-    printf "    2) Option 2\n"
-    printf "    3) Option 3\n"
-    printf "    4) Option 4\n"
-    printf "    5) Option 5\n"
-    printf "    6) Option 6\n"
-    printf "    7) Option 7\n"
-    printf "    8) Option 8\n"
-    printf "    9) Option 9\n"
-    printf "    10) Option 10\n"
-    printf "    11) Option 11\n"
-    printf "    12) Option 12\n"
-    printf "    13) Option 13\n"
-    printf "    14) Option 14\n"
-    printf "    15) Option 15\n"
-    printf "    16) Option 16\n"
-    printf "    17) Option 17\n"
-    printf "    18) Option 18\n"
-    printf "    19) Option 19\n"
-    printf "    20) Option 20\n"
-    printf "    21) Option 21\n"
-    printf "    22) Option 22\n"
-    printf "    23) Option 23\n"
-    printf "    24) Option 24\n"
-    } | pr -t2 -w "$COLUMNS"
-    printf "\n"
-    printf "    x) x to exit\n"
-    printf "    Your choice: "
-    read main
-
-clear
-while [[ "$main" != '\n' ]]; do
-    if [[ "$main" = "\n" ]]; then
-      exit;
-    else
-      case $main in
-    1) init_wordpress; \\n ;;
-    2) clear;
-       opt_2;
-        ;;
-    3) clear;
-       opt_3;
-        ;;
-    4) clear;
-       opt_2;
-        ;;
-    x) clear;
-      exit;
-      ;;
-      \\n) clear;
-      exit;
-      ;;
-    *) clear;
-      option_picked "Pick an option from the menu";
-      main_menu;
-      ;;
-      esac
-    fi
-  done
-}
-option_picked(){
-  message=(${@:-"Error: No message passed"})
-    printf '%s\n' "${message[*]}"
+    printf "$(UI.Color.Yellow)WORDPRESS$(UI.Color.Default)\n"
+    printf "1) Init wp-cli\n"
+    printf "2) Option 2\n"
+    printf "3) Option 3\n"
+    printf "4) Option 4\n"
+    printf "$(UI.Color.Red)0) Exit$(UI.Color.Default)"
+  } | pr -t2 -w "$COLUMNS"
 }
 
-main_menu
+
+read_options(){
+  # try {
+    local choice
+    read -p "Enter choice: " choice
+    case $choice in
+      1) init_wpcli1;;
+      0) exit 0;;
+      *) printf "$(UI.Color.Red)Error...$(UI.Color.Default)" && sleep 2
+    esac
+  # } catch {
+  #   echo "Caught Exception:$(UI.Color.Red) $__BACKTRACE_COMMAND__ $(UI.Color.Default)"
+  #   echo "File: $__BACKTRACE_SOURCE__, Line: $__BACKTRACE_LINE__"
+  #   Exception::PrintException "${__EXCEPTION__[@]}"
+  #   read -p "" fackEnterKey
+  # }
+}
+
+try {
+while true
+do
+	show_menus
+	read_options
+done
+} catch {
+    echo "Caught Exception:$(UI.Color.Red) $__BACKTRACE_COMMAND__ $(UI.Color.Default)"
+    echo "File: $__BACKTRACE_SOURCE__, Line: $__BACKTRACE_LINE__"
+    Exception::PrintException "${__EXCEPTION__[@]}"
+    read -p "" fackEnterKey
+}
