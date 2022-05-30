bf83630763042152626307002a5e5030c940ed9c
tien <tien.codeby@gmail.com>
2022-05-30 19:42:40 +0700

    add ssl

--- scripts/ssl.sh
+++ scripts/ssl.sh
@@ -6,6 +6,7 @@ init_ssl(){
 
 add_ssl_site(){
   printf "$(UI.Color.Yellow)SSL Domain (webb.vn):$(UI.Color.Default)"; read DOMAIN
+  rm -rf /etc/letsencrypt/live/$DOMAIN
   domainPath=/usr/local/lsws/$DOMAIN
   sudo certbot certonly --webroot -w $domainPath/html/ -d $DOMAIN <<<$(printf "tien.wordpress@gmail.com\nA\nn\n")
   printf "/etc/letsencrypt/live/$DOMAIN/fullchain.pem\n"
