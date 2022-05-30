1cc459823f0e8db6a816a77882b3be91e1ee0d76
tien <tien.codeby@gmail.com>
2022-05-30 18:57:13 +0700

    add ssl

--- inits/ssl.sh
+++ inits/ssl.sh
@@ -7,5 +7,5 @@ init_ssl(){
 add_ssl_site(){
   printf "$(UI.Color.Yellow)SSL Domain (webb.vn):$(UI.Color.Default)"; read DOMAIN
   domainPath=/usr/local/lsws/$DOMAIN
-  sudo certbot certonly --webroot -w $domainPath/html/ -d $DOMAIN
+  sudo certbot certonly --webroot -w $domainPath/html/ -d $DOMAIN <<<$(printf "tien.wordpress@gmail.com\nA\nn\n")
 }
