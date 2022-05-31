905564d96a5a880c2a73023ba9f94a597260dbd1
tien <tien.codeby@gmail.com>
2022-05-31 13:51:51 +0700

    add phpmyadmin

--- scripts/lsws.sh
+++ scripts/lsws.sh
@@ -1,6 +1,6 @@
 get_lsws_info(){
   lswsAdminUrl="http://$(hostname -I | cut -d' ' -f1):7080/login.php"
-  lswsAdminPass=`cat $SERVER_ROOT/.litespeed_password`
+  lswsAdminPass=`cat /root/.litespeed_password`
   printf "$(UI.Color.Yellow)LSWS info$(UI.Color.Default)\n"
   printf "$lswsAdminUrl\n"
   printf "$lswsAdminPass\n"
@@ -8,7 +8,7 @@ get_lsws_info(){
   printf " ---\n"
 
   phpMyAdminUrl="http://$(hostname -I | cut -d' ' -f1)/phpmyadmin"
-  phpMyAdminPass==`cat $SERVER_ROOT/.db_password`
+  phpMyAdminPass=`cat /root/.db_password`
   printf "$phpMyAdminUrl\n"
   printf "$phpMyAdminPass\n"
 
