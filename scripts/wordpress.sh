d077d1a571d1161bedeb62a0afec06962b57c3b0
tien <tien.codeby@gmail.com>
2022-06-04 16:14:46 +0700

    wp

--- scripts/wordpress.sh
+++ scripts/wordpress.sh
@@ -53,20 +53,20 @@ add_wpmulti(){
   wp config set SITE_ID_CURRENT_SITE 1 --allow-root
   wp config set BLOG_ID_CURRENT_SITE 1 --allow-root
 
-printf "RewriteEngine On\n
-RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]\n
-RewriteBase /\n
-RewriteRule ^index\.php$ - [L]\n
+# printf "RewriteEngine On
+# RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+# RewriteBase /
+# RewriteRule ^index\.php$ - [L]
 
-# add a trailing slash to /wp-admin\n
-RewriteRule ^([_0-9a-zA-Z-]+/)?wp-admin$ $1wp-admin/ [R=301,L]\n
+# # add a trailing slash to /wp-admin
+# RewriteRule ^([_0-9a-zA-Z-]+/)?wp-admin$ $1wp-admin/ [R=301,L]
 
-RewriteCond %{REQUEST_FILENAME} -f [OR]\n
-RewriteCond %{REQUEST_FILENAME} -d\n
-RewriteRule ^ - [L]\n
-RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]\n
-RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ $2 [L]\n
-RewriteRule . index.php [L]\n" >> $domainPath/.htaccess
+# RewriteCond %{REQUEST_FILENAME} -f [OR]
+# RewriteCond %{REQUEST_FILENAME} -d
+# RewriteRule ^ - [L]
+# RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]
+# RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ $2 [L]
+# RewriteRule . index.php [L]" >> $domainPath/.htaccess
 
   # wp plugin install https://github.com/nguyenshort/codeby-core/archive/refs/heads/master.zip --activate --allow-root
   # wp plugin install /root/webb-bash/assets/elementor-pro-zalo-duy-riba.zip --activate --allow-root
