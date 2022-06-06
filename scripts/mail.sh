9cf3fbed305d5b9d83014edab95c3277a7ddb58a
tien <tien.codeby@gmail.com>
2022-06-06 16:03:36 +0700

    mail

--- scripts/mail.sh
+++ scripts/mail.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 setup_mailjet(){
-  sudo apt install msmtp msmtp-mta -y
-  printf "$(UI.Color.Blue)https://app.mailjet.com/signin$(UI.Color.Default)\n"
-  printf "tiennv.ppe@gmail.com / Pa\$\$w0rd!\n"
-  printf "tien.codeby@gmail.com / mmVT123!@#\n"
-  printf "$(UI.Color.Blue)Add domain$(UI.Color.Default)\n"
-  printf "https://app.mailjet.com/account/sender/domain\n"
-  printf "$(UI.Color.Blue)Cloudflare$(UI.Color.Default)\n"
-  printf "https://dash.cloudflare.com\n"
-  printf "$(UI.Color.Blue)Api key$(UI.Color.Default)\n"
-  printf "https://app.mailjet.com/account/apikeys\n"
-  printf "$(UI.Color.Yellow)Domain (webb.vn):$(UI.Color.Default)"; read DOMAIN
-  printf "$(UI.Color.Yellow)API KEY:$(UI.Color.Default)"; read API_KEY
-  printf "$(UI.Color.Yellow)SECRET KEY:$(UI.Color.Default)"; read SECRET_KEY
+sudo apt install msmtp msmtp-mta -y
+printf "$(UI.Color.Blue)https://app.mailjet.com/signin$(UI.Color.Default)\n"
+printf "tiennv.ppe@gmail.com / Pa\$\$w0rd!\n"
+printf "tien.codeby@gmail.com / mmVT123!@#\n"
+printf "$(UI.Color.Blue)Add domain$(UI.Color.Default)\n"
+printf "https://app.mailjet.com/account/sender/domain\n"
+printf "$(UI.Color.Blue)Cloudflare$(UI.Color.Default)\n"
+printf "https://dash.cloudflare.com\n"
+printf "$(UI.Color.Blue)Api key$(UI.Color.Default)\n"
+printf "https://app.mailjet.com/account/apikeys\n"
+printf "$(UI.Color.Yellow)Domain (webb.vn):$(UI.Color.Default)"; read DOMAIN
+printf "$(UI.Color.Yellow)API KEY:$(UI.Color.Default)"; read API_KEY
+printf "$(UI.Color.Yellow)SECRET KEY:$(UI.Color.Default)"; read SECRET_KEY
 
 printf "defaults\n
 auth on\n
