d826b82738fc37a9b0ea86be9530591013fb8859
tien <tien.codeby@gmail.com>
2022-06-06 22:16:24 +0700

    mail

--- scripts/wordpress.sh
+++ scripts/wordpress.sh
@@ -58,7 +58,7 @@ define( 'PATH_CURRENT_SITE', '/' );
 define( 'SITE_ID_CURRENT_SITE', 1 );
 define( 'BLOG_ID_CURRENT_SITE', 1 );"
 # sed -i "/sendmail_path/a "
-  sed -i '/WP_ALLOW_MULTISITE/a $wpconfig' /var/www/$DOMAIN/wp-config.php
+  sed -i "/WP_ALLOW_MULTISITE/a $wpconfig" /var/www/$DOMAIN/wp-config.php
   read -p "$(UI.Color.Blue)Enter to continue$(UI.Color.Default)" fackEnterKey
 }
 
